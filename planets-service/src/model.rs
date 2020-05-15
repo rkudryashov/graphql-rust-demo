0319f3ff1b35574fe136bc47521bdda4bda5d98b
Roman Kudryashov <rskudryashov@gmail.com>
2020-05-15 17:21:22 +0300

    Add examples of enum, field description, deprecation reason

--- planets-service/src/model.rs
+++ planets-service/src/model.rs
@@ -1,12 +1,15 @@
 use std::collections::HashMap;
 
+use async_graphql::*;
+
 #[derive(Clone)]
 pub struct Planet {
     id: i32,
     name: &'static str,
+    planet_type: Type,
 }
 
-#[async_graphql::Object]
+#[Object]
 impl Planet {
     async fn id(&self) -> i32 {
         self.id
@@ -15,6 +18,24 @@ impl Planet {
     async fn name(&self) -> &str {
         self.name
     }
+
+    #[field(name = "type", desc = "From an astronomical point of view")]
+    async fn planet_type(&self) -> Type {
+        self.planet_type
+    }
+
+    #[field(deprecation = "Now it is not in doubt. Do not use this field")]
+    async fn is_rotating_around_sun(&self) -> bool {
+        true
+    }
+}
+
+#[Enum]
+enum Type {
+    TerrestrialPlanet,
+    GasGiant,
+    IceGiant,
+    DwarfPlanet,
 }
 
 pub struct Storage {
@@ -26,6 +47,7 @@ impl Storage {
         let earth = Planet {
             id: 1,
             name: "Earth",
+            planet_type: Type::TerrestrialPlanet,
         };
 
         let mut planets = HashMap::new();
