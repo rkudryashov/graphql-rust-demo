39d4282555fbed01d0f1829583c780ce4d1b8253
Roman Kudryashov <rskudryashov@gmail.com>
2020-05-16 22:12:27 +0300

    Add apollo-server

---
+++ apollo-server/gateway.js
@@ -0,0 +1,28 @@
+const {ApolloServer} = require("apollo-server");
+const {ApolloGateway, RemoteGraphQLDataSource} = require("@apollo/gateway");
+
+class AuthenticatedDataSource extends RemoteGraphQLDataSource {
+    willSendRequest({request, context}) {
+        request.http.headers.set('Authorization', context.authHeaderValue);
+    }
+}
+
+const gateway = new ApolloGateway({
+    serviceList: [
+        {name: "planets-service", url: "http://localhost:8081"},
+        {name: "satellites-service", url: "http://localhost:8082"}
+    ],
+    buildService({name, url}) {
+        return new AuthenticatedDataSource({url});
+    },
+});
+
+const server = new ApolloServer({
+    gateway, subscriptions: false, context: ({req}) => ({
+        authHeaderValue: req.headers.authorization
+    })
+});
+
+server.listen().then(({url}) => {
+    console.log(`🚀 Server ready at ${url}`);
+});
