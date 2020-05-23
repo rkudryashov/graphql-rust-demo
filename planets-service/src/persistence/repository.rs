6b2f6e55c1e7fb84635843353d55cec80be93935
Roman Kudryashov <rskudryashov@gmail.com>
2020-05-23 18:48:00 +0300

    Use Diesel DSL

--- planets-service/src/persistence/repository.rs
+++ planets-service/src/persistence/repository.rs
@@ -4,8 +4,9 @@ use crate::persistence::model::{DetailsEntity, NewDetailsEntity, NewPlanetEntity
 use crate::persistence::schema::{details, planets};
 
 pub fn all(conn: &PgConnection) -> QueryResult<Vec<(PlanetEntity, DetailsEntity)>> {
-    planets::table
-        .inner_join(details::table)
+    use crate::persistence::schema::{planets::dsl::*, details::dsl::*};
+
+    planets.inner_join(details)
         .load(conn)
 }
 
@@ -16,7 +17,9 @@ pub fn get(id: i32, conn: &PgConnection) -> QueryResult<(PlanetEntity, DetailsEn
 }
 
 pub fn create(new_planet: NewPlanetEntity, mut new_details_entity: NewDetailsEntity, conn: &PgConnection) -> QueryResult<PlanetEntity> {
-    let result: QueryResult<PlanetEntity> = diesel::insert_into(planets::table)
+    use crate::persistence::schema::{planets::dsl::*, details::dsl::*};
+
+    let result: QueryResult<PlanetEntity> = diesel::insert_into(planets)
         .values(new_planet)
         .get_result(conn);
 
@@ -24,7 +27,7 @@ pub fn create(new_planet: NewPlanetEntity, mut new_details_entity: NewDetailsEnt
 
     new_details_entity.planet_id = new_planet_id;
 
-    diesel::insert_into(details::table)
+    diesel::insert_into(details)
         .values(new_details_entity)
         .execute(conn);
 
--- satellites-service/src/persistence/repository.rs
+++ satellites-service/src/persistence/repository.rs
@@ -4,8 +4,9 @@ use crate::persistence::model::SatelliteEntity;
 use crate::persistence::schema::satellites;
 
 pub fn all(conn: &PgConnection) -> QueryResult<Vec<SatelliteEntity>> {
-    satellites::table
-        .load(conn)
+    use crate::persistence::schema::satellites::dsl::*;
+
+    satellites.load(conn)
 }
 
 pub fn get(id: i32, conn: &PgConnection) -> QueryResult<SatelliteEntity> {
@@ -14,10 +15,8 @@ pub fn get(id: i32, conn: &PgConnection) -> QueryResult<SatelliteEntity> {
         .get_result(conn)
 }
 
-pub fn get_by_planet_id(id_of_planet: i32, conn: &PgConnection) -> QueryResult<Vec<SatelliteEntity>> {
-    use crate::persistence::schema::satellites::dsl::*;
-
-    satellites
-        .filter(planet_id.eq(id_of_planet))
+pub fn get_by_planet_id(planet_id: i32, conn: &PgConnection) -> QueryResult<Vec<SatelliteEntity>> {
+    satellites::table
+        .filter(satellites::planet_id.eq(planet_id))
         .load(conn)
 }
