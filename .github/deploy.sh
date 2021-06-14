dc46cca34a480d76cdfc59e68e39a06941128d05
Roman Kudryashov <rskudryashov@gmail.com>
2021-06-14 17:40:40 +0300

    Improve deployment

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -4,10 +4,11 @@ alias docker-compose='docker run --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v "$PWD:$PWD" \
     -w="$PWD" \
-    docker/compose:1.27.4'
+    docker/compose:1.29.2'
 
 cd ~/graphql-rust-demo
 
 docker-compose pull
 docker-compose down
+docker system prune
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
